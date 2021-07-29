75358e3b1d525f72c504152badc4d0e28fa0f6ab
Yorch Sepúlveda <yorch5.77@gmail.com>
2021-07-29 04:55:29 -0400

    Actualizacion (menu principal)
    -> Se reemplaza los Toast por un Alert dialog debido a que los toas dejaron de verse en la aplicaci{on
    -> Creacion de interfaces para el menu principal
    -> LLamar a funciones principales del MainActivity desde el fragmentMenu

--- AppMovil Apoderado/AcuClass/app/src/main/java/com/example/acuclass/MainActivity.kt
+++ AppMovil Apoderado/AcuClass/app/src/main/java/com/example/acuclass/MainActivity.kt
@@ -2,19 +2,64 @@ package com.example.acuclass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.example.acuclass.databinding.ActivityMainBinding
+import com.example.acuclass.interfaces.IcomunicaFragments
 import com.example.acuclass.view.Fragments.MenuFragment
 
-class MainActivity : AppCompatActivity()
-{
+class MainActivity : AppCompatActivity(),IcomunicaFragments {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager.beginTransaction().replace(R.id.contenedorFragments,MenuFragment()).commit()
+        replaceFragment(MenuFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment)
+    {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.contenedorFragments, fragment)
+        fragmentTransaction.commit()
+    }
+
+    override fun verAlumnosNotas()
+    {
+        dialogo("Notas", "Ver notas desde el activity")
+    }
+
+    override fun verAlumnosAsistencia()
+    {
+        dialogo("Asistencia", "Ver asistencia desde el activity")
+    }
+
+    override fun verAlumnosEvaluaciones()
+    {
+        dialogo("Evaluaciones", "Ver evaluaciones desde el activity")
+    }
+
+    override fun configuracion()
+    {
+        dialogo("Configuracion", "Ir a configuracion desde el activity")
+    }
+
+    override fun salir()
+    {
+        dialogo("Salir", "Salir desde el activity")
+    }
+
+    private fun dialogo(titulo:String, mensaje:String)
+    {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(titulo)
+        builder.setMessage(mensaje)
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
--- AppMovil Apoderado/AcuClass/app/src/main/java/com/example/acuclass/view/LoginActivity.kt
+++ AppMovil Apoderado/AcuClass/app/src/main/java/com/example/acuclass/view/LoginActivity.kt
@@ -1,19 +1,19 @@
 package com.example.acuclass.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.core.view.isNotEmpty
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
+import com.example.acuclass.MainActivity
 import com.example.acuclass.R
 import com.example.acuclass.viewmodel.UsuarioViewModel
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity(),LifecycleOwner {
-
     lateinit var correo:TextInputLayout
     lateinit var clave:TextInputLayout
     lateinit var btnIniciar:MaterialButton
@@ -26,9 +26,7 @@ class LoginActivity : AppCompatActivity(),LifecycleOwner {
         clave = findViewById(R.id.claveTextField)
         btnIniciar= findViewById(R.id.btnIniciarSesion)
 
-
         btnIniciar.setOnClickListener{
-
             if(!correo.editText?.text.toString().isEmpty() && !clave.editText?.text.toString().isEmpty())
             {
                 this.viewModel = ViewModelProvider(this).get(UsuarioViewModel::class.java)
@@ -36,18 +34,31 @@ class LoginActivity : AppCompatActivity(),LifecycleOwner {
 
                 if(viewModel.obtenerUsuario())
                 {
-                    Toast.makeText(this,"Usuario encontrado", Toast.LENGTH_LONG).show()
+                    //Toast.makeText(this,"Usuario encontrado", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent);
                 }
                 else
                 {
-                    Toast.makeText(this,"Puede que el email o la clave sean incorrectas o el usuario no se encuentre registrado.", Toast.LENGTH_LONG).show()
+                    dialogo("Datos incorrectos", "Puede que el email o la clave sean incorrectas o el usuario no se encuentre registrado.")
+                    //Toast.makeText(this,"Puede que el email o la clave sean incorrectas o el usuario no se encuentre registrado.", Toast.LENGTH_LONG).show()
                 }
             }
             else
             {
-                Toast.makeText(this,"Los campos de correo y clave no deben estar vacios", Toast.LENGTH_LONG).show()
+                dialogo("Campos vacios", "Los campos de correo y clave no deben estar vacios")
+                //Toast.makeText(this,"Los campos de correo y clave no deben estar vacios", Toast.LENGTH_LONG).show()
             }
         }
     }
 
+    private fun dialogo(titulo:String, mensaje:String)
+    {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(titulo)
+        builder.setMessage(mensaje)
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
 }
